0627ffa6dcd26bd3cf3b5bf516f855cc7ef98fc8
Zeraphim <diamantejc87@gmail.com>
2024-10-30 00:40:05 +0800

    Dashboard Main - Added contents for About Page and Dataset Page

--- dashboard_main.py
+++ dashboard_main.py
@@ -77,12 +77,37 @@ dataset = pd.read_csv("data/IRIS.csv")
 if page_selection == "about":
     st.header("ℹ️ About")
 
+    st.markdown(""" 
+
+    This notebook explores the Iris flower dataset by performing:
+    1. Exploratory Data Analysis (EDA)
+    2. Data Pre-processing
+    3. Training two supervised classification models: **Decision Tree Classifier** and **Random Forest Regressor**
+    4. Making predictions on a new **unseen data**
+
+
+    """)
+
 # Dataset Page
 elif page_selection == "dataset":
     st.header("📊 Dataset")
 
-    st.write("IRIS Flower Dataset")
-    st.write("")
+    st.markdown("""
+
+    The **Iris flower dataset** was introduced by **Ronald Fisher** in 1936, it is a dataset used widely in machine learning. Originally collected by Edgar Anderson, it includes **50 samples** each from three Iris species (**Iris Setosa**, **Iris Virginica**, and **Iris Versicolor**).  
+
+    For each sample, four features are measured: sepal length, sepal width, petal length, and petal width (in centimeters). This dataset is commonly used to test classification techniques like support vector machines. The same dataset that is used for this data science activity was uploaded to Kaggle by the user named **Mathnerd**.
+
+    **Content**  
+    The dataset has **150** rows containing **5 primary attributes** that are related to the iris flower, the columns are as follows: **Petal Length**, **Petal Width**, **Sepal Length**, **Sepal width**, and **Class(Species)**.
+
+    `Link:` https://www.kaggle.com/datasets/arshid/iris-flower-dataset            
+                
+    """)
+
+    # Display the dataset
+    st.subheader("Dataset displayed as a Data Frame")
+    st.dataframe(dataset, use_container_width=True)
 
 # EDA Page
 elif page_selection == "eda":
@@ -100,7 +125,7 @@ elif page_selection == "eda":
         
     with col[2]:
         st.markdown('#### Graphs Column 3')
-        
+
 # Data Cleaning Page
 elif page_selection == "data_cleaning":
     st.header("🧼 Data Cleaning and Data Pre-processing")
